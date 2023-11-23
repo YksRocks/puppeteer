175a683a46eeaa0ad6e9db63490676432a6653d9
YksRocks <73733249+YksRocks@users.noreply.github.com>
2023-11-24 00:48:38 +0530

    data fetching started

--- leader.js
+++ leader.js
@@ -4,36 +4,46 @@ import puppeteer from "puppeteer";
   try {
     const browser = await puppeteer.launch({
       headless: false,
-      userDataDir: "./cache",
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disabled-dev-shm-usage",
-        "--disable-gpu",
-      ],
+      // userDataDir: "./cache",
+      // args: [
+      //   "--no-sandbox",
+      //   "--disable-setuid-sandbox",
+      //   "--disabled-dev-shm-usage",
+      //   "--disable-gpu",
+      // ],
     });
     const page = await browser.newPage();
     await page.goto(
-      "https://www.hackerrank.com/contests/ai-secb-lab3/leaderboard"
+      "https://www.hackerrank.com/contests/ai-secb-lab5/leaderboard"
     );
-    
 
     const link = await page.evaluate(() =>
-      Array.from(document.querySelectorAll("a"), (e) => ({ link: e.href }))
+      Array.from(
+        document.querySelectorAll(
+          "#leaders .leaderboard-list-view .leaderboard-row"
+        ),
+        (e) => ({
+          rank: e.querySelector("div:nth-child(1) p").innerText,
+          username: e.querySelector("div:nth-child(2) .leaderboard-hackername")
+            .innerText,
+          score: e.querySelector("div:nth-child(4) p").innerText,
+          time: e.querySelector("div:nth-child(5) p").innerText,
+        })
+      )
     );
     console.log(link);
 
-    const textS = await page.evaluate(() => {
-      const text = document.querySelectorAll(
-        "#leaders .leaderboard-list-view .row.padding-small.top.bottom.leaderboard-row.row-alt .span-flex-4 p a"
-      );
-      const aa = [];
-      text.forEach((txt) => {
-        aa.push(text.innerText);
-      });
-      return aa;
-    });
-    console.log(textS);
+    // const textS = await page.evaluate(() => {
+    //   const text = document.querySelectorAll(
+    //     "#leaders .leaderboard-list-view .leaderboard-hackername"
+    //   );
+    //   const aa = [];
+    //   text.forEach((txt) => {
+    //     aa.push(text.innerText);
+    //   });
+    //   return aa;
+    // });
+    // console.log(textS);
 
     // const textS = await page.evaluate(() =>
     //   Array.from(
@@ -50,7 +60,6 @@ import puppeteer from "puppeteer";
     //   }))
     // );
     // console.log(textS);
-
 
     await browser.close();
   } catch (e) {
